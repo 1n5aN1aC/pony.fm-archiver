cd187d464442b21ecf5034fcb5219ba535911f0f
1n5aN1aC <joshua@mailmag.net>
2019-09-18 18:48:56 -0700

    Add some comments

--- downloadSongs.py
+++ downloadSongs.py
@@ -10,22 +10,28 @@ currentFile = ""
 currentNum = 1
 
 try:
+	# For each item in the pickle...
 	for key, value in dict.items():
 		streams = value['streams']
 		dlLink = streams['mp3']
 		track  = str(value['id'])
 		
+		# Check if file exists (already downloaded)
 		currentFile = "dl/" + track + ".mp3"
 		if not os.path.isfile(currentFile):
+			# OK then, actually download the song now!
 			try:
 				testfile = urllib.request.URLopener()
 				urllib.request.urlretrieve(dlLink, currentFile )
 				print (track + " done! (" + str(currentNum) + " of " + str(len(dict)) + ")")
 			except Exception:
 				try:
+					# Not sure why, but there's actually a lot of corrupt songs.
+					# I contacted the site owner, but they didn't seem to care?
 					print (track + " failed: " + value['title'])
 				except Exception:
 					print ("Printing the name of the file that failed has failed.")
 		currentNum += 1
+# If you ctrl-C it, remove the partially-downloaded file.
 except KeyboardInterrupt:
 	os.remove(currentFile)
